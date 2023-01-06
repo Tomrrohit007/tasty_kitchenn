bcdda9432d28fefef31aab7d91429b849de5a39d
Tomrrohit007 <tomarrohit5034@gmail.com>
2023-01-06 21:20:34 +0530

    error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import {Route, Switch, Redirect} from 'react-router-dom'
+import CowinDashboard from './components/CowinDashboard'
 
-import LoginForm from './components/LoginForm'
-import Home from './components/Home'
-import Products from './components/Products'
-import ProductItemDetails from './components/ProductItemDetails'
-import Cart from './components/Cart'
-import NotFound from './components/NotFound'
-import ProtectedRoute from './components/ProtectedRoute'
 import './App.css'
 
-const App = () => (
-  <Switch>
-    <Route exact path="/login" component={LoginForm} />
-    <ProtectedRoute exact path="/" component={Home} />
-    <ProtectedRoute exact path="/products" component={Products} />
-    <ProtectedRoute exact path="/products/:id" component={ProductItemDetails} />
-    <ProtectedRoute exact path="/cart" component={Cart} />
-
-    <Route path="/not-found" component={NotFound} />
-    <Redirect to="not-found" />
-  </Switch>
-)
+const App = () => <CowinDashboard />
 
 export default App
